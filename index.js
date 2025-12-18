78bfd33730133b76113b0c08f42cb757f9133bd9
arbazkhan97 <arbazkhan97432@gmail.com>
2025-12-18 18:23:29 +0530

    initial coommit

---
+++ index.js
@@ -0,0 +1,42 @@
+
+// for( let i=0;i<=10;i++){
+//     console.log(i)
+// }
+// console.log(i)  //  10
+
+
+
+// for( var i=0;i<=10;i++){
+//     console.log(i)
+// }
+// console.log(i) 
+
+
+// iffi
+// (()=>{
+//    var a=10;
+//     console.log(a);
+// }) ();
+//  var a=20
+// console.log(a)
+
+
+// hoisting
+// hello();
+
+//  let hello=function hello(){
+//     console.log(hello)
+// }
+
+
+
+//  
+
+// let arr=[1,2,3,4]
+
+// let [a,b,c,d]=arr;  //  array de-structuring 
+// console.log(a ,b,c,d);
+
+
+// console.log([]==[]) //   type conversion     ""> 0 == 0 => tr ==  ""  =ue 
+// console.log([]==![]) //  ![] false ""=> 0   true 
